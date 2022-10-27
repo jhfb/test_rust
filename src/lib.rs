f7d577477edf79d7be132a5a8a8fe5647b2a7492
jhfb <2335062719@qq.com>
2022-10-27 10:43:54 +0800

    '1'

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod hello;
+
+#[cfg(test)]
+mod tests {
+    use crate::hello;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+        hello::hello_world();
+    }
+}
